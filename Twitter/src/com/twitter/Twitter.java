f715815d0d697cb125d145e4c0936c1d3f384a7f
damljanovicdunja <damljanovicdunja96@gmail.com>
2017-04-06 15:22:00 +0200

    Grenerisane JavaDoc HTML strane.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -2,15 +2,31 @@
 package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
-
+/**
+ * 
+ * @author Dunja Damjanovic
+ *@version 1.1
+ *Klasa vrsi unos twitova u listu Poruke ,na kraj same liste
+ * i pretragu liste za porukama da odgovarajucim tagom i vraca niz pronadjenih poruka
+ *  ako broj poruka ne prelazi unapred odredjenu duzinu niza.
+ */
 public class Twitter {
-
+	/**
+	 * Lista objekata tipa TwitterPoruka.
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
-
+	/**
+	 * Vraca celokupnu listu "poruke" koju cine objekti tipa TwitterPoruka.
+	 * @return listu objekata tipa TwitterPoruka.
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
-
+	/***
+	 * Unosi novu poruku na kraj liste "poruke".
+	 * @param korisnik kao novo ime korisnika.
+	 * @param poruka kao novi tekst poruke.
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -19,7 +35,18 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
-
+	/**
+	 * Pretrazuje poruke koje sadrze odredjeni tag, i odgovarajuce puni
+	 *  u niz koji potom vraca kao povratnu vrednost.
+	 * @param maxBroj kao maksimalna duzina niza.
+	 * @param tag kao kriterijum za pretrazivanje poruka.
+	 * @return niz objekata tipa TwitterPoruka koji sadrzi poruke nadjene prema zadatom tagu.
+	 * @throws java.lang.RuntimeException ako je uneti tag:
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazan String</li>
+	 * </ul>
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
 			throw new RuntimeException("Morate uneti tag");
